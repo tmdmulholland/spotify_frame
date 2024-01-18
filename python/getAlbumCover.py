95ef42d71b02697a00b15a57d9d08477a370d1ca
Thomas Mulholland <tmdmulholland@gmail.com>
2024-01-18 19:28:07 +0000

    collect now playing image link

--- python/getAlbumCover.py
+++ python/getAlbumCover.py
@@ -1,24 +1,16 @@
-import os
 from dotenv import load_dotenv
-
-load_dotenv()
-
-# spotipy_client_id = os.getenv('CLIENT_ID')
-# spotipy_client_secret = os.getenv('CLIENT_SECRET')
-# spotipy_redirect_uri = os.getenv('SPOTIPY_REDIRECT_URI')
-
-
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
 
-birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
+load_dotenv()
+
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
-results = spotify.artist_albums(birdy_uri, album_type='album')
-albums = results['items']
-while results['next']:
-    results = spotify.next(results)
-    albums.extend(results['items'])
+results = spotify.current_user_playing_track()
 
-for album in albums:
-    print(album['name'])
+print(results)
+
+song = results['item']['album']
+imageURL = results['item']['album']['images'][2]['url']
+print(song)
+print(imageURL)
--- python/getToken.py
+++ python/getToken.py
@@ -1,22 +1,10 @@
 from dotenv import load_dotenv
-import os
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+from spotipy.oauth2 import SpotifyOAuth
 
 load_dotenv()
+def getToken():
+    scope = 'user-read-currently-playing'
+    auth = SpotifyOAuth(scope=scope, open_browser=False)
+    token = auth.get_access_token()
 
-client_id = os.getenv("CLIENT_ID")
-client_secret = os.getenv("CLIENT_SECRET")
-
-# def getToken():
-scope = 'user-read-currently-playing'
-
-    # This way removes the need for a browser, it will instead give the URL to visit in the terminal
-    # auth = SpotifyOAuth(scope=scope, open_browser=False)
-    # token = auth.get_access_token()
-
-
-auth_manager = SpotifyClientCredentials()
-sp = spotipy.Spotify(auth_manager=auth_manager)
-
-now_playing = sp.current_user_playing_track()
+getToken()
